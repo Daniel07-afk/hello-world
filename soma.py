a552422bbec31fa56a28826f789d9db1c77cdb71
DS7 <p.danielsalvador@gmail.com>
2025-04-30 18:19:34 +0000

    criação de variaveis numero1 e numero2 e opção soma

--- soma.py
+++ soma.py
@@ -1,10 +1,15 @@
+#variaveis para números
+# int essa recebe valores inteiros(1,2,3...)
+# float recebe como essas(1,2-1,3-3,14...)
 
 
 
+numero1 = int(input("Digite o primeiro número :"))
+#print(numero1)
 
+numero2 = float(input("Digite o segundo número :"))
+#print(numero2)
 
-numero1 = input("Digite o primeiro número")
-print(numero1)
+soma = numero1 + numero2
 
-numero2 = input("Digite o segundo número")
-print(numero2)
+print("o resultado da soma dos dois números é : ",soma)
