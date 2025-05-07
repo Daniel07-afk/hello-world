013fd44b6eae13b4fb2a45233e2ea0fdf0f48916
DS7 <p.danielsalvador@gmail.com>
2025-05-07 18:17:08 +0000

    revisão da materia passada

---
+++ revisao.py
@@ -0,0 +1,17 @@
+#comentario
+
+#variavel é um espaço na memória que receberá dado
+#tipo : texto, número (int e float(moeda,altura,peso))
+
+#exemplo de número float (flutuante)
+peso = 89.5
+print(peso)
+
+#exemplo de número inteiros
+idade = 18
+print(idade)
+
+#exemplo de variavel do tipo texto
+nome = ("maria") 
+sobrenome = "silva"
+print(nome,sobrenome)
