3eb3459a57d7a47f1509eea2664182fea0465056
DS7 <p.danielsalvador@gmail.com>
2025-05-08 18:39:12 +0000

    execicio de multiplicação

---
+++ multiplicacao.py
@@ -0,0 +1,10 @@
+numero1 = int(input("digite o primeiro numero:"))
+
+numero2 = int(input("digite o segundo numero:"))
+
+numero3 = int(input("digite o terceiro numero:"))
+
+numero4 = int(input("digite o quarto numero:"))
+
+multiplicacao = (numero1 * numero2 * numero3 * numero4)
+print("o resultado da conta é:", multiplicacao)
