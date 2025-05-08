1ccdf38918cd7d525cd725d861ae89121367700b
DS7 <p.danielsalvador@gmail.com>
2025-05-08 18:31:22 +0000

    exercicio de subtração

---
+++ subtracao.py
@@ -0,0 +1,11 @@
+numero1 = int(input("digite o primeiro numero:"))
+
+numero2 = int(input("digite o segundo numero:"))
+
+numero3 = int(input("digite o terceiro numero:"))
+
+numero4 = int(input("digite o quarto numero:"))
+
+subtracao = (numero1 - numero2 - numero3 - numero4)
+print("o resultado da conta é:", subtracao)
+
